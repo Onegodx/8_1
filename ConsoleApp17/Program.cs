91ac54e82be69dbf86b499ca06547b17d12a8317
Onegodx <113604166+Onegodx@users.noreply.github.com>
2023-06-27 00:30:24 +0200

    Update Program.cs

--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.IO;
-
+//Миньков Владимир 14 вариант базовый уровень 8_1
 class Program
 {
     static void Main(string[] args)
